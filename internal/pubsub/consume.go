fdb6a352c92d88015499e6fcf483e987b9461d8d
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-24 13:48:31 +0700

    feat: add dead letter exchange configuration for queue declaration

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -40,7 +40,9 @@ func DeclareAndBind(
 		simpleQueueType != SimpleQueueDurable, // delete when unused
 		simpleQueueType != SimpleQueueDurable, // exclusive
 		false,                                 // no-wait
-		nil,                                   // arguments
+		amqp.Table{
+			"x-dead-letter-exchange": "peril_dlx",
+		},
 	)
 	if err != nil {
 		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
